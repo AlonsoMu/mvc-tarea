e8bf32f5df6f07ef6438dc64a6e77eebc0fad502
Alonso Muñoz <alonsomunoz263@gmail.com>
2023-04-05 19:41:29 -0600

    Formulario y Listar completos

---
+++ controllers/producto.controller.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once '../models/producto.php';
+
+if (isset($_POST['operacion'])){
+
+  $producto = new Producto();
+
+  if($_POST['operacion'] == 'listar'){
+
+    $datosObtenidos = $producto->listarProductos();
+    //En esta ocasion NO enviaremos un objeto JSON, en su lugar el controlador renderizara las filas que necesita <tobdy></tobdy>
+    //echo json_encode($datosObtenidos);
+
+    //PASO 1: Verificar que el objeto contenga datos
+    if($datosObtenidos){
+      $numeroFila = 1;
+      // PASO 2: Recorrer todo el objeto
+      foreach($datosObtenidos as $producto){
+        // PASO 3: Ahora construimos las filas
+        echo "
+        <tr>
+          <td>{$numeroFila}</td>
+          <td>{$producto['nombreproducto']}</td>
+          <td>{$producto['modelo']}</td>
+          <td>{$producto['marca']}</td>
+          <td>{$producto['color']}</td>
+          <td>{$producto['conectividad']}</td>
+          <td>{$producto['peso']}</td>
+          <td>{$producto['fecharegistro']}</td>
+          <td>{$producto['precio']}</td>
+          <td> 
+          <a href='#' data-idproducto='{$producto['idproducto']}' class='btn btn-danger btn-sm eliminar'><i class='bi bi-trash3-fill'></i></a>
+                <a href='#' data-idproducto='{$producto['idproducto']}' class='btn btn-info btn-sm editar'><i class='bi bi-pencil-fill'></i></a>
+          </td>
+        </tr>
+        ";
+        $numeroFila++;
+      }
+    }
+
+  }
+
+  if($_POST['operacion'] == 'registrar'){
+
+    //Paso 1: Recpger los datos que nos envia la vista (FORM,utilizando ajax)
+    $datosForm = [
+      "nombreproducto"      => $_POST['nombreproducto'], //CLAVES // VALORES
+      "modelo"              => $_POST['modelo'],
+      "marca"               => $_POST['marca'],
+      "color"               => $_POST['color'],
+      "conectividad"        => $_POST['conectividad'],
+      "peso"                => $_POST['peso'],
+      "fecharegistro"       => $_POST['fecharegistro'],
+      "precio"              => $_POST['precio']
+    ];
+
+    //Paso 2: Enviar el arreglo como paramentro del metodo
+      $producto->registrarProducto($datosForm);
+
+  }
+
+  if($_POST['operacion'] == 'eliminar'){
+    $curso->eliminarProducto($_POST['idproducto']);
+  }
+}
---
+++ models/Producto.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once "Conexion.php";
+
+//MODELO = contiende la logica
+//extends : Herencia (PDO) en PHP
+class Producto extends Conexion{
+
+  // Objeto que almacene la conexion que viene desde el padre(Conexion) y la compartira con todos los metodos (CRUD+)
+  private $accesoBD;
+
+  //Constructor, INICIALIZAR
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion(); // El valor de retorno (getConexion) de esta funcion ha sido asignada a este objeto (accesoBD)
+  }
+
+  //Metodo listar cursos
+  public function listarProductos(){
+    try {
+      // 1. Preparamos la consulta
+      $consulta = $this->accesoBD->prepare("CALL spu_productos_listar()");
+      // 2. Ejecutamos la consulta
+      $consulta->execute();
+      // 3. Devolvemos la consulta(array asociativo)
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+
+  public function registrarProducto($datos = []){
+    try {
+      //1. Preparamos la consulta
+      $consulta = $this->accesoBD->prepare("CALL spu_productos_registrar(?,?,?,?,?,?,?,?)");
+      //2. Ejecutamos la consulta
+      $consulta->execute(
+        array(
+          $datos["nombreproducto"],
+          $datos["modelo"],
+          $datos["marca"],
+          $datos["color"],
+          $datos["conectividad"],
+          $datos["peso"],
+          $datos["fecharegistro"],
+          $datos["precio"]
+        )
+      );
+      
+    }
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function eliminarProducto($idproducto = 0){
+    try {
+      $consulta = $this->accesoBD->prepare("CALL spu_productos_eliminar(?)");
+      $consulta->execute(array($idproducto));
+    }
+    catch (Exception $e) {
+      die($e->getMessage());
+    }
+  }
+
+
+
+}
